b8dec3c21cd8cb56279ac3bcf46c8ca1b2c03792
Rodolfo Miranda <rsilvamiranda91@gmail.com>
2021-05-06 21:00:30 -0300

    Criando a pasta partials do projeto e criando as rotas para capturar os dados do formulário

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //estou dizendo para o Express usar o ejs como viem engine
 app.set('view engine','ejs');
 app.use(express.static('public')); //pastas aonde ficam os arquivos estaticos.
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/",(req, res) => {
- 
     res.render("index");
- 
 });
 
 app.get("/perguntar",(req,res)=>{
     res.render("perguntar");
 })
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+res.send("Formulario recebido! titulo " + titulo + " " + "descricao" + descricao);
+});
 
 app.listen(8080,()=>{
     console.log("APP RODANDO!!!");
